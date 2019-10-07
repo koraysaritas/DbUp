e764c538e13cbcc5086dc58e4c01a70843af58b2
koraysaritas <koraysaritas@hotmail.com>
2019-10-07 15:28:49 +0300

    Add support Altibase DB.

---
+++ src/dbup-altibase/AltibaseCommandSplitter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace DbUp.Altibase
+{
+    public class AltibaseCommandSplitter
+    {
+        /// <summary>
+        /// Splits a script with multiple delimited commands into commands
+        /// </summary>
+        /// <param name="scriptContents"></param>
+        /// <returns></returns>
+        public IEnumerable<string> SplitScriptIntoCommands(string scriptContents)
+        {
+            using (var reader = new AltibaseCommandReader(scriptContents))
+            {
+                var commands = new List<string>();
+                reader.ReadAllCommands(c => commands.Add(c));
+                return commands;
+            }
+        }
+    }
+}
